20bae201eead0ce8a4d4a78f2570c167fd6c4580
ayush97710 <ayushkr97710@gmail.com>
2025-10-19 20:56:53 +0530

    Update public directory with latest files

--- public/assets/js/projects.js
+++
@@ -1,6 +0,0 @@
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if(entry.isIntersecting) entry.target.classList.add("show");
-  });
-});
-observer.observe(document.querySelector(".projects-cards"));
